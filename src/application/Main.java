9e8a5cabc01edabeaeb1555eb88639582cb8b914
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-24 11:08:31 +0300

    GUI

---
+++ src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage window) {
+        Button button = new Button("This is a button");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+
+        Scene scene = new Scene(componentGroup);
+
+        window.setScene(scene);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(Main.class);
+    }
+}
