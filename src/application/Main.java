85dbe781886ff5d4d579e9e45a97e13be6a1e5b8
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-24 14:51:16 +0300

    Vocabulary practice

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,70 +2,58 @@ package application;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage window) throws Exception {
+    private Dictionary dictionary;
 
-        // 1. Create main layout
+    @Override
+    public void init() throws Exception {
+        // 1. Create the dictionary that the application uses
+        this.dictionary = new Dictionary();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // 2. Create the views ("subviews")
+        PracticeView practiceView = new PracticeView(dictionary);
+        InputView inputView = new InputView(dictionary);
+
+        // 3. Create the higher level layout
         BorderPane layout = new BorderPane();
 
-        // 1.1. Create menu for main layout
+        // 3.1. Create the menu for the general layout
         HBox menu = new HBox();
         menu.setPadding(new Insets(20, 20, 20, 20));
         menu.setSpacing(10);
 
-        // 1.2. Create buttons for menu
-        Button first = new Button("Joke");
-        Button second = new Button("Answer");
-        Button third = new Button("Explanation");
+        // 3.2. Create the menu buttons
+        Button enterButton = new Button("Enter new words");
+        Button practiceButton = new Button("Practice");
 
-        // 1.3. Add buttons to menu
-        menu.getChildren().addAll(first, second, third);
-
+        // 3.3. Add the buttons to the menu
+        menu.getChildren().addAll(enterButton, practiceButton);
         layout.setTop(menu);
 
 
-        // 2. Add subviews and add them to the menu buttons
-        // 2.1. Create subview layout
-        StackPane firstLayout = createView("Why did the old man fall down the well?");
-        StackPane secondLayout = createView("He couldn't see that well.");
-        StackPane thirdLayout = createView("Old men don't usually have great eyesight so he fell down the well.");
+        // 4. Connect the subviews with the buttons. Clicking menu buttons changes the subview.
+        enterButton.setOnAction((event) -> layout.setCenter(inputView.getView()));
+        practiceButton.setOnAction((event) -> layout.setCenter(practiceView.getView()));
 
-        // 2.2. Add subviews to button. Pressing the buttons will change the view
-        first.setOnAction((event) -> layout.setCenter(firstLayout));
-        second.setOnAction((event) -> layout.setCenter(secondLayout));
-        third.setOnAction((event) -> layout.setCenter(thirdLayout));
+        // 5. First show the input view
+        layout.setCenter(inputView.getView());
 
-        // 2.3. Set initial view
-        layout.setCenter(firstLayout);
+        // 6. Create the main view and add the high level layout
+        Scene view = new Scene(layout, 400, 300);
 
-        // 3. Create main scene with layout
-        Scene scene = new Scene(layout);
-
-
-        // 4. Show the main scene
-        window.setScene(scene);
-        window.show();
-    }
-
-    private StackPane createView(String text) {
-
-        StackPane layout = new StackPane();
-        layout.setPrefSize(300, 180);
-        layout.getChildren().add(new Label(text));
-        layout.setAlignment(Pos.CENTER);
-
-        return layout;
+        // 7. Show the application
+        stage.setScene(view);
+        stage.show();
     }
 
     public static void main(String[] args) {
