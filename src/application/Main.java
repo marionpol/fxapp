a8710fe1a8b0f9de04830ae98a51d197264c251f
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-24 13:41:06 +0300

    Multiple views

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,37 +2,61 @@ package application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-
 import javafx.stage.Stage;
-
-import java.util.Scanner;
+import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
-    private static String title;
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the title: ");
-        title = scanner.nextLine();
-
-        scanner.close();
-
-        launch(args);
-    }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle(title);
+    public void start(Stage window) {
 
-        Label titleLabel = new Label(title);
+        Label firstView = new Label("First View");
+        BorderPane layout = new BorderPane();
 
-        VBox layout = new VBox(20);
-        layout.getChildren().add(titleLabel);
+        Button first = new Button("To the second view");
+        layout.setTop(firstView);
+        layout.setCenter(first);
 
-        Scene scene = new Scene(layout, 300, 100);
-        primaryStage.setScene(scene);
+        VBox secondVbox = new VBox();
+        Button second = new Button("To the third view");
+        Label secondView = new Label("Second View");
+        secondVbox.getChildren().add(second);
+        secondVbox.getChildren().add(secondView);
 
-        primaryStage.show();
+        GridPane grid = new GridPane();
+        Label thirdView = new Label("Third View");
+        grid.add(thirdView, 0, 0);
+        Button third = new Button("To the first view");
+        grid.add(third, 1, 1);
+
+
+        Scene firstV = new Scene(layout);
+        Scene secondV = new Scene(secondVbox);
+        Scene thirdV = new Scene(grid);
+
+        first.setOnAction((event) -> {
+            window.setScene(secondV);
+        });
+
+        second.setOnAction((event) -> {
+            window.setScene(thirdV);
+        });
+
+        third.setOnAction((event) -> {
+            window.setScene(firstV);
+        });
+
+
+
+        window.setScene(firstV);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(Main.class);
     }
 }
